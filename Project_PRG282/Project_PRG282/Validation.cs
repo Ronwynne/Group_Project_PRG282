6bf263ce0566066783211b45a60b33c60bd20f72
Ronwynne <578580@student.belgiumcampus.ac.za>
2022-11-10 10:41:17 +0200

    Login Form Validation

--- Project_PRG282/Project_PRG282/Validation.cs
+++ Project_PRG282/Project_PRG282/Validation.cs
@@ -8,5 +8,34 @@ namespace Project_PRG282
 {
     class Validation
     {
+        public Validation()
+        {
+        }
+
+        //Login Form Validation
+        public bool LogInVal(string username, string password)
+        {
+            if (username != string.Empty && password != string.Empty)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool newUserVal(string username, string password, string confirmPass)
+        {
+            if (username != string.Empty && password != string.Empty && confirmPass != string.Empty)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
     }
 }
